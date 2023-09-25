c60daa7291996849e4a4fcd61e88f7796484a8ff
Arsen1y-dev <arsenij-perfilov@mail.ru>
2023-09-25 22:19:54 +0400

    Update 13.09.23.py

--- 13.09.23.py
+++ 13.09.23.py
@@ -376,23 +376,23 @@
 #        print("Ошибка: Введите номер действия числом (1, 2 или 3).")
 
 
-## Задание 1.3* Генератор сказок
-#
-#
-## Запрос информации о сказке
-#hero_name = input("Как зовут героя? ")
-#villain_name = input("Кто его противник? ")
-#kingdom_name = input("Название места действия? ")
-#conflict_reason = input("Причина конфликта? ")
-#hero_weapon = input("Оружие героя? ")
-#villain_ability = input("Способность злодея? ")
-#years = input("Сколько лет герою? ")
-#
-## Вывод текста сказки с подстановками
-#print(f"Жил-был в королевстве {kingdom_name} герой {hero_name}. Было ему {years} лет.")
-#print(f"Однажды на королевство напал злой {villain_name}.")
-#print(f"Причина конфликта была следующей: {conflict_reason}.")
-#print(f"Герой {hero_name} вооружен {hero_weapon}, а злодей {villain_name} обладает ужасной способностью: {villain_ability}.")
-#print(f"День бились, другой бились, наконец {hero_name} одолел врага.")
-#print(f"Вернулся домой, а {hero_name} забыл {hero_weapon} в лесу.")
-#print(f"Тут и сказочке конец, а кто слушал — молодец!")
+# Задание 1.3* Генератор сказок
+
+
+# Запрос информации о сказке
+hero_name = input("Как зовут героя? ")
+villain_name = input("Кто его противник? ")
+kingdom_name = input("Название места действия? ")
+conflict_reason = input("Причина конфликта? ")
+hero_weapon = input("Оружие героя? ")
+villain_ability = input("Способность злодея? ")
+years = input("Сколько лет герою? ")
+
+# Вывод текста сказки с подстановками
+print(f"Жил-был в королевстве {kingdom_name} герой {hero_name}. Было ему {years} лет.")
+print(f"Однажды на королевство напал злой {villain_name}.")
+print(f"Причина конфликта была следующей: {conflict_reason}.")
+print(f"Герой {hero_name} вооружен {hero_weapon}, а злодей {villain_name} обладает ужасной способностью: {villain_ability}.")
+print(f"День бились, другой бились, наконец {hero_name} одолел врага.")
+print(f"Вернулся домой, а {hero_name} забыл {hero_weapon} в лесу.")
+print(f"Тут и сказочке конец, а кто слушал — молодец!")
