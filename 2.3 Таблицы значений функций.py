524a6e8e4f18dc86eb89a20cf2837a6327caf5ba
Arsen1y-dev <arsenij-perfilov@mail.ru>
2023-10-11 11:34:50 +0400

    Update 2.3 Таблицы значений функций.py

--- 2.3 Таблицы значений функций.py
+++ 2.3 Таблицы значений функций.py
@@ -6,3 +6,11 @@
 ## Таблица значений функции 8 (1)
 #
 #
+a, b, h = map(float, input().split())
+
+for x in [round(a + i * h, 6) for i in range(int((b - a) / h) + 1)]:
+    try:
+        y = (3 * x + 4) /(x**2 + 2 * x + 1)**0.5
+        print(f'{x:.6f}\t{y:.6f}')
+    except ValueError:
+        print(f'{x:.6f}\tundefined')
