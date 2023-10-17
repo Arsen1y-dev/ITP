501b62d417b5ac4a7f34e31f046ce9a3b7083377
Arsen1y-dev <arsenij-perfilov@mail.ru>
2023-10-17 23:52:21 +0400

    Update 2.3 Таблицы значений функций.py

--- 2.3 Таблицы значений функций.py
+++ 2.3 Таблицы значений функций.py
@@ -8,9 +8,20 @@
 #
 a, b, h = map(float, input().split())
 
-for x in [round(a + i * h, 6) for i in range(int((b - a) / h) + 1)]:
+# Используйте round для точных сравнений с плавающей точкой
+a = round(a, 6)
+b = round(b, 6)
+h = round(h, 6)
+
+# Используйте round для корректного сравнения чисел с плавающей точкой
+x = a
+while x <= b:
     try:
-        y = (3 * x + 4) /(x**2 + 2 * x + 1)**0.5
+        y = (3 * x + 4) / ((x**2 + 2 * x + 1) ** 0.5)
         print(f'{x:.6f}\t{y:.6f}')
-    except ValueError:
+    except (ValueError, ZeroDivisionError):
         print(f'{x:.6f}\tundefined')
+    x = round(x + h, 6)
+
+
+
