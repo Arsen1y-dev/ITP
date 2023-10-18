e87a28a1aafa2c8bac6bf4d655e3e1c9a6e03bfb
Arsen1y-dev <arsenij-perfilov@mail.ru>
2023-10-18 11:29:41 +0400

    Update 2.3 Таблицы значений функций.py

--- 2.3 Таблицы значений функций.py
+++ 2.3 Таблицы значений функций.py
@@ -6,22 +6,75 @@
 ## Таблица значений функции 8 (1)
 #
 #
+#a, b, h = map(float, input().split())
+#
+## Используйте round для точных сравнений с плавающей точкой
+#a = round(a, 6)
+#b = round(b, 6)
+#h = round(h, 6)
+#
+## Используйте round для корректного сравнения чисел с плавающей точкой
+#x = a
+#while x <= b:
+#    try:
+#        y = (3 * x + 4) / ((x**2 + 2 * x + 1) ** 0.5)
+#        print(f'{x:.6f}\t{y:.6f}')
+#    except (ValueError, ZeroDivisionError):
+#        print(f'{x:.6f}\tundefined')
+#    x = round(x + h, 6)
+# 
+#
+#
+##Таблица значений функции 13 (2)
+#
+#
+##
+##
+##from math import log
+##
+##a, b, h = map(float, input().split())
+##
+##a = round(a, 6)
+##b = round(b, 6)
+##h = round(h, 6)
+##
+##x = a
+##while x <= b:
+##    try:
+##        if x > 2:
+##            y = log(x - 2) / ((5 * x + 1) ** 0.5)
+##            print(f'{x:.6f}\t{y:.6f}')
+##        else:
+##            print(f'{x:.6f}\tundefined')
+##    except ValueError:
+##        print(f'{x:.6f}\tundefined')
+##    x = round(x + h, 6)
+#
+#
+#
+##Таблица значений функции 17 (3)
+#
+#
+from math import sqrt
+
 a, b, h = map(float, input().split())
 
-# Используйте round для точных сравнений с плавающей точкой
 a = round(a, 6)
 b = round(b, 6)
 h = round(h, 6)
 
-# Используйте round для корректного сравнения чисел с плавающей точкой
 x = a
 while x <= b:
     try:
-        y = (3 * x + 4) / ((x**2 + 2 * x + 1) ** 0.5)
-        print(f'{x:.6f}\t{y:.6f}')
-    except (ValueError, ZeroDivisionError):
+        if x > 2:
+            y = ((x + 4) / x ** 2 - 2) + sqrt(x ** 3 - 1)
+            print(f'{x:.6f}\t{y:.6f}')
+        else:
+            print(f'{x:.6f}\tundefined')
+    except ValueError:
         print(f'{x:.6f}\tundefined')
     x = round(x + h, 6)
 
 
 
+    
