8976eaf575e91988f39b1b7862a3cb4a14dca611
Arseniy Perfilov <arsenij-perfilov@mail.ru>
2023-11-08 00:21:48 +0400

    Update 4.1 Строки.py

--- 4.1 Строки.py
+++ 4.1 Строки.py
@@ -22,3 +22,34 @@
 ## Используем срез [::-1], чтобы получить обратное слово, затем выводим каждую букву в отдельной строке
 #for letter in word[::-1]:
 #    print(letter)
+#
+#
+#
+## Буквы на четных местах в обратном порядке
+#
+#
+#s1 = input()
+#s2 = s1[1::2]
+#reversed_str = ''.join(reversed(s2))
+#print(reversed_str)
+#
+#
+#
+## Строки. Задача 19
+#
+#
+#def is_symmetric(s):
+#    return s == s[::-1]
+#
+#s = input()
+#
+#if is_symmetric(s):
+#    print(1)
+#else:
+#    print(0)
+#
+#
+#
+## Строки. Задача 3
+#
+#
