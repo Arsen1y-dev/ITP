1d242d4e47a0f6cdb0eb6e03223b67ec4ba9cbca
Arseniy Perfilov <129983483+Arsen1y-dev@users.noreply.github.com>
2024-03-10 19:27:32 +0400

    19.27 10.03 частично сделал school + гдз хендбук и 1 творческое

---
+++ 2 Семестр/Задание 7.1 Множества- цифры.py
@@ -0,0 +1,9 @@
+numbers = input().split()
+
+for number in numbers:
+    unique_digits = set(number)
+    result = []
+    for digit in unique_digits:
+        if number.count(digit) % 2 == 0:
+            result.append(digit)
+    print(''.join(result))
